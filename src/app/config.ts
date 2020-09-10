b81fa5ceab7cf6ed78ffc727c71ad77e4a2b4cc4
JorgeQuijano <totti.quijano@gmail.com>
2020-09-09 23:28:49 -0400

    forgot the s in https

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const config = {
     // apiUrl: 'http://192.168.0.12:3000'
     // apiUrl: 'http://35.203.60.252/api/'
-    apiUrl: 'http://fussdata.com/api/'
+    apiUrl: 'https://fussdata.com/api/'
   };
